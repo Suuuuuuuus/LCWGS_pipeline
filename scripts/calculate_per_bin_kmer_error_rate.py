09d633088246f08838cd664a4a856fdab4a67b18
Suuuuuuuus <bjzzp01@163.com>
2023-06-13 00:19:00 +0100

    Fixation

--- scripts/calculate_per_bin_kmer_error_rate.py
+++ scripts/calculate_per_bin_kmer_error_rate.py
@@ -14,14 +14,12 @@ import itertools
 import collections
 
 code = sys.argv[1]
-read = sys.argv[2]
+strand = sys.argv[2]
 
-def calculate_per_bin_kmer_error_rate(code, read, intervals = [50,100,150,200,250,300,350,400,450,500,550,600]):
-    result = pd.read_csv('results/per_bin_kmer/'+code+'_subsampled/read'+read+'_by_length/read_'+read+'.txt', sep = ' ', 
-                         usecols = ['read_id', 'number_of_kmers_at_threshold', 'number_of_solid_kmers_at_threshold'])
+def calculate_per_bin_kmer_error_rate(code, strand, intervals = [50,100,150,200,250,300,350,400,450,500,550,600]):
+    result = pd.read_csv('results/per_bin_kmer/'+code+'_subsampled/read'+strand+'_by_length/read_'+strand+'.txt', sep = ' ', usecols = ['read_id', 'number_of_kmers_at_threshold', 'number_of_solid_kmers_at_threshold'])
     result['read_id'] = result['read_id'].str.split().str[0]
-    bins = pd.read_csv('results/per_bin_kmer/'+code+'_subsampled/trimmed_'+read+'.tsv', sep = '\t', header = None, names = ['read_id', 'bin'],
-                      dtype={'read_id': 'string', 'bin': 'int'})
+    bins = pd.read_csv('results/per_bin_kmer/'+code+'_subsampled/trimmed_'+strand+'.tsv', sep = '\t', header = None, names = ['read_id', 'bin'], dtype={'read_id': 'string', 'bin': 'int'})
     df = pd.merge(result, bins, on='read_id', how="left")
 
     intervals = intervals
@@ -33,6 +31,6 @@ def calculate_per_bin_kmer_error_rate(code, read, intervals = [50,100,150,200,25
     error_rate_ary.append(error_rate)
 
     res = pd.DataFrame({code: error_rate_ary}).T
-    res.to_csv('results/per_bin_kmer/'+code+'_subsampled/read'+read+'_by_length/per_bin_kmer_error_rate_read'+read+'.txt', header=False, sep='\t')
+    res.to_csv('results/per_bin_kmer/'+code+'_subsampled/read'+strand+'_by_length/per_bin_kmer_error_rate_read'+strand+'.txt', header=False, sep='\t')
 
-calculate_per_bin_kmer_error_rate(code, read)
+calculate_per_bin_kmer_error_rate(code, strand)
