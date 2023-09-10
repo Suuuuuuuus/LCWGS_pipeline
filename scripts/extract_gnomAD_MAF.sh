6e65db9e62a49bba64c79e92cb3bf23917d8968c
Suuuuuuuus <bjzzp01@163.com>
2023-09-10 20:06:23 +0100

    Fixation

--- scripts/extract_gnomAD_MAF.sh
+++ scripts/extract_gnomAD_MAF.sh
@@ -3,7 +3,7 @@
 #SBATCH -A ansari.prj
 #SBATCH -p short
 #SBATCH -o output.out
-#SBATCH -D /well/ansari/users/gjx698/low_coverage_sequencing/scripts
+#SBATCH -D /well/ansari/users/gjx698/low_coverage_sequencing
 #SBATCH -c 4
 #SBATCH --array=1-22
 
@@ -12,8 +12,10 @@ module load Anaconda3/2022.05
 eval "$(conda shell.bash hook)"
 conda activate sus
 
-zgrep -v '#' "../data/gnomAD_vcf/gnomad.genomes.v3.1.2.sites.chr${SLURM_ARRAY_TASK_ID}.vcf.bgz" | \
+mkdir -p "results/variant_calling/gnomAD_MAFs/"
+
+zgrep -v '#' "data/gnomAD_vcf/gnomad.genomes.v3.1.2.sites.chr${SLURM_ARRAY_TASK_ID}.vcf.bgz" | \
 awk -v "OFS=\t" '{ $8=$8; sub(/^.*AF=/, "", $8); sub(/;.*/, "", $8); print $1,$2,$4,$5,$8 }' | \
 awk '$5 !~ /^[[:alpha:]]/' | \
 awk '!(length($3)>1 || length($4)>1)' | \
-awk '!($5==0.00000)' > "../results/variant_calling/gnomAD_MAFs/gnomAD_MAF_chr${SLURM_ARRAY_TASK_ID}.txt"
+awk '!($5==0.00000)' > "results/variant_calling/gnomAD_MAFs/gnomAD_MAF_chr${SLURM_ARRAY_TASK_ID}.txt"
