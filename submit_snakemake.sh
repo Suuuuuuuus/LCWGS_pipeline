64ce2cc95a5aa92d6499c32c549b465b7e0888a9
Suuuuuuuus <bjzzp01@163.com>
2023-10-21 22:15:09 +0100

    Fixation

--- submit_snakemake.sh
+++ submit_snakemake.sh
@@ -2,8 +2,7 @@
 # $2 = rules to run
 cores=$1
 rule="$2"
-option="${3}"
 
-snakemake -s pipelines/master.smk -c "$cores" --profile slurm/ "$rule" "$option"
+snakemake -s pipelines/master.smk -c "$cores" --profile slurm/ "$rule"
 
 #snakemake --forceall --rulegraph | dot -Tpdf > dag.pdf
