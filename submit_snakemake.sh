b300f81718d86a1380f3193df7c51e9cd07b7ef9
Suuuuuuuus <bjzzp01@163.com>
2023-10-25 11:17:15 +0100

    Rewrite submission code

--- submit_snakemake.sh
+++ submit_snakemake.sh
@@ -1,8 +1,15 @@
-# $1 = number of cores
-# $2 = rules to run
-cores=$1
-rule="$2"
-
-snakemake -s pipelines/master.smk -c "$cores" --profile slurm/ "$rule"
-
-#snakemake --forceall --rulegraph | dot -Tpdf > dag.pdf
+if [ "$#" -eq 1 ]; then
+    snakemake --forceall --rulegraph | dot -Tpdf > dag.pdf
+elif [ "$#" -eq 2 ]; then
+    cores="$1"
+    rule="$2"
+    snakemake -s pipelines/master.smk -c "$cores" --profile slurm/ "$rule"
+elif [ "$#" -eq 3 ]; then
+    cores="$1"
+    rule="$2"
+    option="$3"
+    snakemake -s pipelines/master.smk -c "$cores" "$option" --profile slurm/ "$rule"
+else
+    echo "Invalid inputs."
+    exit 1  # Exit with an error status
+fi
