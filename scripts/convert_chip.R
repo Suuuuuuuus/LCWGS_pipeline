293d53c724df89d937dc2cf6e24ae6b246bca927
Suuuuuuuus <bjzzp01@163.com>
2024-03-03 19:27:54 +0000

    Fixation

--- scripts/convert_chip.R
+++ scripts/convert_chip.R
@@ -37,26 +37,27 @@ parse_args = function() {
 
 args = parse_args()
 
-#echo( "++ Loading manifest annotation from %s...\n", args$manifest )
+echo( "++ Loading manifest annotation from %s...\n", args$manifest )
 annot.db = dbConnect( dbDriver( "SQLite" ), args$manifest ) 
 annotation = as_tibble( dbGetQuery( annot.db, "SELECT * FROM Annotations" ))
-# It turns out all PMRA SNPs are annotated on the + strand, which is helpful:
+# It turns out all PMRA SNPs are annotated on the + strand, which is helpful.
+# However let's check this here:
 stopifnot( length( which( annotation$Strand != '+' )) == 0)
-#echo( "++ Ok, loaded %d annotation records.\n", nrow(annotation))
-# echo(
-	# "++ %d records had Stop != Start, max length was %d...\n",
-	# length( which( annotation$Start != annotation$Stop )),
-	# max( annotation$Stop - annotation$Start, na.rm = T )
-# )
+echo( "++ Ok, loaded %d annotation records.\n", nrow(annotation))
+echo(
+	"++ %d records had Stop != Start, max length was %d...\n",
+	length( which( annotation$Start != annotation$Stop )),
+	max( annotation$Stop - annotation$Start, na.rm = T )
+)
 
-# echo( "++ Loading genotypes from %s...\n", args$genotypes )
+echo( "++ Loading genotypes from %s...\n", args$genotypes )
 X = read_tsv( args$genotypes, comment = '#' )
 
-# echo( "++ Loading samples from %s...\n", args$samples )
 metadata = X[,c(1,188:212)]
 G = as.matrix( X[,2:187])
 colnames(G) = gsub( ".CEL_call_code", "", colnames(G))
 
+echo( "++ Loading samples from %s...\n", args$samples )
 samples = read_tsv( args$samples, comment = '#' )
 samples$sample_id = gsub( ".CEL", "", samples[[1]], fixed = T )
 M = match( colnames(G), samples$sample_id )
@@ -64,57 +65,59 @@ stopifnot( length( which( is.na( M ))) == 0 )
 stopifnot( length( M ) == ncol(G) )
 samples = samples[M,]
 
-# echo( "++ Ok, loaded genotypes for %d samples and %d variants.\n", nrow(samples), nrow(G))
+echo( "++ Ok, loaded genotypes for %d samples and %d variants.\n", nrow(samples), nrow(G))
 
 # Match up genotypes to annotation
-# echo( "++ Matching genotypes to annotations...\n" )
+echo( "++ Matching genotypes to annotations...\n" )
 M = match( metadata$probeset_id, annotation$ProbeSet_ID )
 stopifnot( length( which( is.na(M))) == 0 )
-# echo( "++ Ok, all variants matched.\n" )
+echo( "++ Ok, all variants matched.\n" )
 
 matched.annotation = annotation[M,]
 
-# We EXCLUDE a few records that are apparently for multi-allelic
+# We IGNORE a few records that are apparently for multi-allelic
 # variants, but which have two records with the second expressed as two alt alleles.
-# => hard to process sensibly without further work.  (There are only 3)
-wNoRef = which( matched.annotation$Ref_Allele == '.' | is.na( matched.annotation$Ref_Allele ) )
-# echo( "++ Excluding %d variants with . ref allele, (usually means multiple alleles).\n", length( wNoRef ))
+# => hard to process sensibly without further work.
+info = tibble( ProbeSet_ID = matched.annotation$ProbeSet_ID, result = NA )
+info$result[wNoRef] = "no_ref_allele"
 
-if( length(wNoRef) > 0 ) {
-	G = G[-wNoRef,]
-	matched.annotation = matched.annotation[-wNoRef,]
-	metadata = metadata[-wNoRef,]
-}
+wNoRef = which( matched.annotation$Ref_Allele == '.' | is.na( matched.annotation$Ref_Allele ) )
+echo( "++ I will ignore %d variants with . ref allele, (usually means multiple alleles).\n", length( wNoRef ))
+
 
 # Make sure all remaining variants are either ref/alt or alt/ref
 {
 	w = which(
 		# Not ref/alt
-		( matched.annotation$Ref_Allele != matched.annotation$Allele_A | matched.annotation$Alt_Allele != matched.annotation$Allele_B )
+		( matched.annotation$Ref_Allele == matched.annotation$Allele_A & matched.annotation$Alt_Allele == matched.annotation$Allele_B )
 		# and not alt/ref
-		& ( matched.annotation$Ref_Allele != matched.annotation$Allele_B | matched.annotation$Alt_Allele != matched.annotation$Allele_A )
+		| ( matched.annotation$Ref_Allele == matched.annotation$Allele_B & matched.annotation$Alt_Allele == matched.annotation$Allele_A )
 	)
-	stopifnot( length(w) == 0 )
+	stopifnot( length(w) == nrow(matched.annotation) - length(wNoRef))
 }
 
-allele.sets = unique( annotation[, c( "Ref_Allele", "Alt_Allele")] )
-# print( allele.sets )
+allele.sets = unique( matched.annotation[, c( "Ref_Allele", "Alt_Allele")] )
+print( allele.sets )
 
 echo( "++ Translating genotype calls to VCF format...\n" )
 translated = G
+translated[,] = "./."
+counts = c( total = 0, fail = 0, noref = length( wNoRef ) )
+
 for( i in 1:nrow( allele.sets )) {
 	set = allele.sets[i,]
-	# echo( "  --: %s...\n", paste( set, collapse = " > " ))
+	echo( "  --: %s...\n", paste( set, collapse = " > " ))
 
 	translation = c()
 	translation[sprintf( "%s/%s", set$Ref_Allele, set$Ref_Allele )] = '0/0'
 	translation[sprintf( "%s/%s", set$Ref_Allele, set$Alt_Allele )] = '0/1'
 	translation[sprintf( "%s/%s", set$Alt_Allele, set$Ref_Allele )] = '0/1'
 	translation[sprintf( "%s/%s", set$Alt_Allele, set$Alt_Allele )] = '1/1'
-	translation["---"] = "./."
+	translation[ "---"] = "./."
 
 	w = which(
-		matched.annotation$Ref_Allele == set$Ref_Allele
+		!is.na( matched.annotation$Ref_Allele )
+		& matched.annotation$Ref_Allele == set$Ref_Allele
 		& matched.annotation$Alt_Allele == set$Alt_Allele
 	)
 	for( j in w ) {
@@ -125,11 +128,47 @@ for( i in 1:nrow( allele.sets )) {
 			}
 		)
 	}
+	info$result[w] = "ok"
+	{
+		fails = unique( which( is.na( translated[w,,drop=F] ), arr.in = TRUE )[,1])
+		info$result[fails] = "fail"
+
+		if( length(fails) > 0 ) {
+			echo( "!! there were %d failures of %d variants (%.1f%%).\n", length(fails), length(w), 100 * length(fails) / length(w))
+		}
+		translated[w,][is.na(translated[w,])] = "./."
+		counts['total'] = counts['total'] + length(w)
+		counts['fail'] = counts['fail'] + length(fails)
+	}
+	echo(
+		"++ ...processed %d variants so far, of which %d (%.1f%%) had annotation-data mismatches.\n",
+		counts['total'],
+		counts['fail'],
+		100 * counts['fail'] / counts['total']
+	)
 }
 
-print(translated)
+echo(
+	"++ Ok, counts were: %d total, %d fail, %d no reference allele.\n",
+	counts['total'],
+	counts['fail'],
+	counts['noref']
+)
+
+echo( "++ Ok, writing info output...\n" )
+info$ref_allele = matched.annotation$Ref_Allele
+info$alt_allele = matched.annotation$Alt_Allele
+write_tsv( info, file = gsub( '[.]vcf', '.info.tsv', args$output, ))
 
 echo( "++ Ok, forming VCF output...\n" )
+# We remove missing-ref-allele variants here
+# As I don't think there can be a missing ref allele in VCF output.
+if( length(wNoRef) > 0 ) {
+	G = G[-wNoRef,]
+	matched.annotation = matched.annotation[-wNoRef,]
+	metadata = metadata[-wNoRef,]
+}
+
 vcf.data = tibble(
 	CHROM = matched.annotation$Chr_id,
 	POS = matched.annotation$Start,
@@ -208,4 +247,5 @@ write_tsv(
 	append = TRUE
 )
 echo( "++ Success.\n" )
+echo( "++ Thanks for using convert.R\n" )
 
